6d884a31ee99d9fe0139297f4ce29e039cf84bde
user224 <toda7.mk3@gmail.com>
2019-10-31 21:36:12 +0900

    2019-10-31 ５回目のコミット　6-1-5 不要な弾を破棄してみよう

---
+++ Assets/VRShooting/Scripts/AutoDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+    [SerializeField] float lifetime = 5f; //ゲームオブジェクトの寿命 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
