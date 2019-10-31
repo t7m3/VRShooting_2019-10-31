430069db6497380e27e5948b1c162836ffdab640
user224 <toda7.mk3@gmail.com>
2019-10-31 15:22:36 +0900

    2019-10-31(木)　最初のコミット　script Exampleまで

---
+++ Assets/VRShooting/Scripts/Example.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //最初に一度メッセージを表示する
+        Debug.Log("[Start]");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Spaceキーが押されている間メッセージを表示する
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("[Update] Space key pressed");
+        }
+    }
+}
